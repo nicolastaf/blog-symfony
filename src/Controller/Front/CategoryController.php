78ed14c8600c7506d6b9f4b05ea1deb918dd6370
nicolastaf <nicolas.staffent@gmail.com>
2023-02-15 18:32:40 +0100

    Add pagination and design change

--- src/Controller/Front/CategoryController.php
+++ src/Controller/Front/CategoryController.php
@@ -6,9 +6,11 @@ use App\Entity\Post;
 use App\Entity\Category;
 use App\Repository\PostRepository;
 use App\Repository\CategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class CategoryController extends AbstractController
 {
@@ -26,12 +28,22 @@ class CategoryController extends AbstractController
     /**
      * @Route("/category/{id}-{slug}", name="category_show", methods={"GET"}, requirements={"id"="\d+"})
      */
-    public function show(Category $category, Post $posts = null, CategoryRepository $categoryRepository, PostRepository $postRepository): Response
+    public function show(
+        Category $category, 
+        Post $posts = null, 
+        CategoryRepository $categoryRepository,
+        PaginatorInterface $paginator, 
+        PostRepository $postRepository,
+        Request $request): Response
     {
         // Get all categories
         $categories = $categoryRepository->findAll();
         // Get the posts by category
-        $posts = $postRepository->findByCategory(['post' => $posts]);
+        $posts = $paginator->paginate(
+            $postRepository->findByCategory(['post' => $posts]),
+            $request->query->getInt('page', 1), /*page number*/
+            5 /*limit per page*/
+        );
         //dd($posts);
         $category = $categoryRepository->find($category);     
 
